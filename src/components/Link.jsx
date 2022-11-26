2a5b812a86db1e6d4e2ab844ec565e1f0cb6d9a1
Amri <amri.arshad16@yahoo.com>
2022-11-26 15:02:05 +1300

    links open in new tab

--- src/components/Link.jsx
+++ src/components/Link.jsx
@@ -3,7 +3,7 @@ import React from "react";
 const Link = ({ name, src }) => {
   return (
     <div className="link">
-      <a href={src}>
+      <a href={src} target="_blank">
         <button>{name}</button>
       </a>
     </div>
